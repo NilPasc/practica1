9b468a80088060e3b799f564b8ad17dc3c613559
NilPasc <146719342+NilPasc@users.noreply.github.com>
2023-10-19 12:56:18 +0200

    Update practica1NIL.sh

--- practica1NIL.sh
+++ practica1NIL.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+echo "Sortir (q)"   
+echo "Llistar països (lp)"
+echo "Seleccionar país (sc)"
+echo "Seleccionar estat (se)"
+echo "LListar els estats del país seleccionat (le)"
+echo "Llistar les poblacions del país seleccionat (lcp)"
+echo "Extreure les poblecions del país seleccionat (ecp)"
+echo "Llistar les poblacions de l’estat seleccionat (lce)"
+echo "Extreure les poblacions de l’estat seleccionat (ce)"
+echo "Obtenir dades d’una ciutat de la WikiData (gwd)"
+echo "Obtenir estadístiques (est)"
 echo Introduce una opción ; read opcion 
 codi_pais='aws'
 codi_ciudad='xx'
@@ -44,6 +55,45 @@ do
 	;;
 	"le")
 		cut -d ',' -f1,2,7 cities.csv | grep -w $codi_pais | cut -d',' -f1,2 | column -s ', ' 
+	;;
+        "lcp")
+                cut -d ',' -f2,7,11 cities.csv | grep -w $codi_pais | cut -d',' -f1,3
+        ;;
+        "ecp")
+                if [[ "$codi_pais" != "xx" ]]
+                then
+                    cut -d ',' -f2,7,11 cities.csv | grep -w $codi_pais | cut -d',' -f1,3 > "archpractica1.csv/$codi_pais.csv"
+                    echo "Se ha generado el archivo codi_pais.csv correctamente."
+                else
+                    echo "Error: Para generar el archivo debes seleccionar un pais con la opción "sc"."
+                fi
+        ;;
+        "lce")
+                cut -d ',' -f2,4,7,11 cities.csv | grep -w $codi_pais | grep -w $codi_estat | cut -d ',' -f1,4
+        ;;
+        "ce")
+                cut -d ',' -f2,4,7,11 cities.csv | grep -w $codi_pais | grep -w $codi_estat | cut -d ',' -f1,4 > "archpractica1.csv/${codi_pais}_${codi_estat}.csv"
+        ;;
+        "gwd")
+                echo "Nombre de la población" ; read poblacio
+                wdid="$(cut -d ',' -f2,4,7,11 cities.csv | grep -w $codi_pais | grep -w $codi_estat | grep -w $poblacio |  cut -d ',' -f4)"
+        if [[ -z "$wdid" ]] 
+                then
+                        echo "Esta poblacion no obstenta WikiDataId o no pertecene al pais o estado seleccionados"
+                else
+                        curl https://www.wikidata.org/wiki/Special:Entity Data//$wdid.json > "archivosjasonp1.csv/$wdid.json"
+        fi   
+	"est")
+                nord="$(awk -F',' '{ if ($9 > 0) nord +=1 } END { print nord }' cities.csv)"
+                sur="$(awk -F',' '{ if ($9 < 0) sur +=1 } END { print sur} ' cities.csv)"
+                est="$(awk -F',' '{ if ($10 > 0) oriental +=1 } END { print oriental }' cities.csv)"
+                oest="$(awk -F',' '{ if ($10 < 0) occidental +=1 } END { print occidental }'
+cities.csv)"
+                NoUbic="$(awk -F',' '{ if ($10 == 0 && $9 == 0) NoUbic +=1 } END { print NoUbic }'
+cities.csv)"
+                NoWiki="$(awk -F',' '{ if ($11 == '') NoWiki +=1 } END { print NoWiki }' cities.csv)"
+        echo Nord $nord Sur $sur Est $est Oest $oest NoUbic $NoUbic No WDId $NoWiki            
+        ;;
 		
 	esac
 	echo $codi_pais
